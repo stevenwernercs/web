bacec5ab7656433ba723122b2dc929ec8d83bd06
stevenwernercs <stevenwernercs.gmail.com>
2015-09-21 22:17:58 -0500

    Added new pages & beer

---
+++ nameSite/main/tweets.php
@@ -0,0 +1,78 @@
+<?php
+$pageTitle="Steven:tweets";
+$pageName="tweets";
+require_once("../inc/header.inc");
+?>
+
+ <!-- static Carousel
+    ================================================== -->
+    <br/>
+    <br/>
+        
+ <!-- START THE FEATURETTES -->
+  <div class="container marketing">
+     
+  <br/>
+  <br/>
+ 
+      <div class="row featurette">
+	<div class="col-md-5">
+	  <a class="twitter-timeline" href="https://twitter.com/StevenWernerCS" data-widget-id="633017232902107136">Your device is unable to display the twitter widget: click to open my timeline @StevenWernerCS</a>
+          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
+	<div class="col-md-6 col-md-offset-1">
+        <h2 class="featurette-heading">Featured tweets:</h2>
+<blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr">Waiting for the time where i can recycle water bottles, plastic bags, and product packaging into 3D printable filament at my house...</p>&mdash; Steven Werner (@StevenWernerCS) <a href="https://twitter.com/StevenWernerCS/status/645811115474087937">September 21, 2015</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr">What is in the way of truly intelligent AI?&#10;&quot;NLP holds most if not all the answers to this question.&quot;&#10;<a href="https://t.co/1C4tMTXxTD">https://t.co/1C4tMTXxTD</a></p>&mdash; Steven Werner (@StevenWernerCS) <a href="https://twitter.com/StevenWernerCS/status/645805352504549376">September 21, 2015</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr">Gartner looks at when smart things rule the world <a href="https://twitter.com/hashtag/digitalbusiness?src=hash">#digitalbusiness</a> <a href="https://twitter.com/hashtag/GartnerSCC?src=hash">#GartnerSCC</a> <a href="http://t.co/glCnqsmJBV">http://t.co/glCnqsmJBV</a> <a href="http://t.co/kpORYzLBtP">pic.twitter.com/kpORYzLBtP</a></p>&mdash; Gartner (@Gartner_inc) <a href="https://twitter.com/Gartner_inc/status/632240980226392065">August 14, 2015</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr"><a href="https://twitter.com/Gartner_inc">@Gartner_inc</a> <a href="https://twitter.com/hashtag/almostthere?src=hash">#almostthere</a>&#10;When smart rules: container itself knows its empty, gets a drone ride and refills self after sharing usage data</p>&mdash; Steven Werner (@StevenWernerCS) <a href="https://twitter.com/StevenWernerCS/status/632250875575054336">August 14, 2015</a></blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>	
+	</div>
+      </div>
+
+      <hr class="featurette-divider">
+      <!-- /END THE FEATURETTES -->
+<?php /*
+
+<hr>
+<h3>What product search could be</h3>
+<p id="facets"></p>
+
+<input type="text" class="form-control" id="search" onkeyup="myFunction()" />
+<script>
+function myFunction() {
+    var gender = ["men", "mens", "men's", "man", "male", "boys", "boy", "woman", "womans", "woman's", "girls", "girl's", "girl", "female"]
+    var sport = ["soccer", "football", "tennis", "basketball", "running", "golf", "baseball"]
+    var color = ["red", "blue", "black", "white", "orange", "green", "yellow"]
+    var product = ["socks", "shoes", "cleats", "ball"]
+    var x = document.getElementById("search").value.toLowerCase().replace(/\W+/g, ' ');
+    var keywords = x.split(" ");
+    var facet = ""
+    for (key in keywords) {
+      console.log(keywords[key]);
+      if ($.inArray(keywords[key], gender) >= 0) {
+	facet += "gender ";
+      } else if ($.inArray(keywords[key], sport) >= 0) {
+	facet += "sport ";
+      } else if ($.inArray(keywords[key], color) >= 0) {
+	facet += "color ";
+      } else if ($.inArray(keywords[key], product) >= 0) {
+	facet += "product ";
+      } else {
+      	facet += "... ";
+      }
+    }
+    document.getElementById("facets").innerHTML = facet;
+}
+</script>
+*/
+?>
+
+<?php include_once("../inc/footer.inc"); ?>       
+  
