75d28a6c4583c15cca1fb17c7a7c74f0a98c4cf6
stevenwernercs <stevenwernercs.gmail.com>
2016-10-04 21:22:23 -0500

    added new g+ vanity url

--- nameSite/main/contact.php
+++ nameSite/main/contact.php
@@ -16,11 +16,12 @@ require_once("../inc/header.inc");
             <p style="font-size:40px">contact<b>werner</b></p>
 	    <p>
 	      <a href="https://twitter.com/StevenWernerCS"           target="_blank"><img src="../img/icon/Twitter-icon.png"  alt"twitter" title="tw://stevenWernerCS"/></a>
-	      <a href="https://plus.google.com/106384600588420199346/posts/p/pub"    target="_blank"><img src="../img/icon/GooglePlus-icon.png"  alt"google+" title="g+ doesn't provide a vanity URL... :("/></a>
+	      <a href="https://plus.google.com/+StevenWernerCS"    target="_blank"><img src="../img/icon/GooglePlus-icon.png"  alt"google+" title="g+://stevenWernerCS"/></a>
+	      <!-- a href="https://plus.google.com/106384600588420199346/posts/p/pub"    target="_blank"><img src="../img/icon/GooglePlus-icon.png"  alt"google+" title="g+ doesn't provide a vanity URL... :("/></a -->
 	      <a href="http://www.linkedin.com/in/stevenWernerCS"    target="_blank"><img src="../img/icon/LinkedIn-icon.png" alt"linkedin" title="in://stevenWernerCS"/></a>
 	      <a href="https://github.com/stevenWernerCS"            target="_blank"><img src="../img/icon/GitHub-icon.png"  alt"github" title="gh://stevenWernerCS"/></a>
   	      <a href="http://www.facebook.com/stevenWernerCS"       target="_blank"><img src="../img/icon/Facebook-icon.png"  alt"facebook" title="fb://stevenWernerCS"/></a>
-	      <a href="skype:StevenWernerCS?call"><img src="../img/icon/Skype-icon.png"  alt"skype" title="../img/icon/skype:stevenWernerCS"/></a>
+	      <a href="skype:StevenWernerCS?call"><img src="../img/icon/Skype-icon.png"  alt"skype" title="skype:stevenWernerCS"/></a>
 	    </p>
 	    <p>
 	      <a href="mailto:stevenWernerCS@gmail.com"><img
